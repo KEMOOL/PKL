b0ba6a44eae240e67f371748f70f85830438407a
KEMOOL <ahmadmufid06@gmail.com>
2020-10-13 05:11:28 +0700

    fix tanggal helper

--- application/helpers/tanggal_helper.php
+++ application/helpers/tanggal_helper.php
@@ -8,49 +8,9 @@
 if (!function_exists('bulan')) {
     function bulan()
     {
+        $arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
         $bulan = Date('m');
-        switch ($bulan) {
-            case 1:
-                $bulan = "Januari";
-                break;
-            case 2:
-                $bulan = "Februari";
-                break;
-            case 3:
-                $bulan = "Maret";
-                break;
-            case 4:
-                $bulan = "April";
-                break;
-            case 5:
-                $bulan = "Mei";
-                break;
-            case 6:
-                $bulan = "Juni";
-                break;
-            case 7:
-                $bulan = "Juli";
-                break;
-            case 8:
-                $bulan = "Agustus";
-                break;
-            case 9:
-                $bulan = "September";
-                break;
-            case 10:
-                $bulan = "Oktober";
-                break;
-            case 11:
-                $bulan = "November";
-                break;
-            case 12:
-                $bulan = "Desember";
-                break;
-
-            default:
-                $bulan = Date('F');
-                break;
-        }
+        $bulan = $arrayBulan[$bulan - 1];
         return $bulan;
     }
 }
